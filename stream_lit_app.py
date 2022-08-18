e4fe6180a133faf99933ac8dd0b1ae55ff7deaaa
Chandrababu <111487843+chandrababu-ETLdeveloper@users.noreply.github.com>
2022-08-18 17:19:18 +0530

    Update stream_lit_app.py
    streamlit library to display the pulled my_fruits_list from pandas library

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -8,8 +8,9 @@
 ##streamlit.text('🍘''Ricecracker')
 ##streamlit.header('🍉🍎Build your own fruits smoothie🥑🍒')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-##streamlit.dataframe(my_fruit_list)
+##import pandas
+##my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") ##pull the data to my_fruits_list using pandas##
+import streamlit
+streamlit.dataframe(my_fruit_list) ##asking streamlit library to display the list of pulled my_fruits_list##
 
 
