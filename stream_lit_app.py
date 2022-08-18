32e88f40ddba64e1cfc000e06c9d634abef3d43f
Chandrababu <111487843+chandrababu-ETLdeveloper@users.noreply.github.com>
2022-08-18 17:13:51 +0530

    Update stream_lit_app.py
    Added pandas library to get the csv file data from s3 bucket

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -1,4 +1,15 @@
-import streamlit
-streamlit.title('My Parents Healthy Diner')
-streamlit.header('🍉🍎Build your own fruits smoothie🥑🍒')
+##import streamlit
+
+##streamlit.title('My Parents Healthy Diner')
+##streamlit.header('Breakfast menus')
+##streamlit.text('🥪sandwich 3 & roti')
+##streamlit.text('🥣kale,spinach & dhal')
+##streamlit.text('🥚🥚Egg')
+##streamlit.text('🍘''Ricecracker')
+##streamlit.header('🍉🍎Build your own fruits smoothie🥑🍒')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
