ed121014a9dc03c55216f4c5332f731371c98b48
Chandrababu <111487843+chandrababu-ETLdeveloper@users.noreply.github.com>
2022-08-18 17:20:13 +0530

    Update stream_lit_app.py

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -10,7 +10,8 @@
 
 ##import pandas
 ##my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") ##pull the data to my_fruits_list using pandas##
-import streamlit
-streamlit.dataframe(my_fruit_list) ##asking streamlit library to display the list of pulled my_fruits_list##
+##import streamlit
+streamlit.dataframe(my_fruit_list) 
+##asking streamlit library to display the list of pulled my_fruits_list##
 
 
