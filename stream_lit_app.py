2e158c9442b764afe4293b4ee37a8af41d1642d2
Chandrababu <111487843+chandrababu-ETLdeveloper@users.noreply.github.com>
2022-08-18 13:33:19 +0530

    Update stream_lit_app.py
    added the breakfast menu

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('chapathi 3 & roti')
+streamlit.text('kale,spinach & dhal')
+streamlit.Text('Egg')
