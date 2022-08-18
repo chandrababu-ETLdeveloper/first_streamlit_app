5249724f531896e67f58e80d6e03fe237fa77605
Chandrababu <111487843+chandrababu-ETLdeveloper@users.noreply.github.com>
2022-08-18 17:37:28 +0530

    Update stream_lit_app.py

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -11,8 +11,14 @@
 ##import pandas
 ##my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") ##pull the data to my_fruits_list using pandas##
 ##import streamlit
-##import pandas
-streamlit.dataframe(my_fruit_list);
+import pandas
+##streamlit.dataframe(my_fruit_list);
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 
 
 
